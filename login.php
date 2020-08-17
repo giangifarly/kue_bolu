3b2142658082c501a661ddecb327184c8a3f02bc
giangifarly <gifarlygian@gmail.com>
2020-08-17 19:44:10 +0700

    Menghubungkan Web

--- login.php
+++ login.php
@@ -1,28 +1,31 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-    <link rel="stylesheet" href="login.css">
-  </head>
-  <body>
-<form action="proses_login.php" method="POST"><div class="login-box">
-  <h1>Login</h1>
-  <div class="textbox">
-    <i class="fas fa-user"></i>
-    <input name="username" type="text" placeholder="Username">
-  </div>
 
-  <div class="textbox">
-    <i class="fas fa-lock"></i>
-    <input name="password" type="password" placeholder="Password">
-  </div>
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <link rel="stylesheet" href="assets/css/login.css">
+</head>
 
-  <input type="submit" class="btn" value="Sign in">
+<body>
+  <form action="proses_login.php" method="POST">
+    <div class="login-box">
+      <h1>Login</h1>
+      <div class="textbox">
+        <i class="fas fa-user"></i>
+        <input name="username" type="text" placeholder="Username">
+      </div>
 
-  <a href="signup.php" style="font-style: none;"> <input type="button" class="btn" value="Sign Up"> </a> 
-</div>
-</form>
+      <div class="textbox">
+        <i class="fas fa-lock"></i>
+        <input name="password" type="password" placeholder="Password">
+      </div>
 
-  </body>
+      <input type="submit" class="btn" value="Sign in">
+
+      <a href="signup.php" style="font-style: none;"> <input type="button" class="btn" value="Sign Up"> </a>
+    </div>
+  </form>
+
+</body>
 </html>
--- signup.php
+++ signup.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title></title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
     <link href="https://fonts.googleapis.com/css?family=Permanent+Marker" rel="stylesheet">
   </head>
   <body>
@@ -22,43 +22,5 @@
       </a> 
     </form>
     </div>
-
-<?php
-require 'conn.php';
-if (isset($_POST['submit'])) {
-$user=$_POST['userbaru'];
-$pass1=$_POST['passbaru1'];
-$pass2=$_POST['passbaru2'];
-
-$data= mysqli_query($mysqli, "SELECT * FROM akun WHERE username='$user'" );
-$row = mysqli_fetch_array($data);
-if ($row > 0) {
-  error_reporting(0);
-  echo "Username telah di gunakan";
-  exit;
-}else {
-
-  if ($pass1==$pass2) {
-    
-$passnew = sha1($pass1);
-
-$data2 = mysqli_query($mysqli,"INSERT INTO `akun` (`username`,`password`) VALUES ('$user','$passnew');");
-
-if ($data2) {
-    //Jika berhasil
-    echo "Data Admin Telah Ditambakan";
-    header("location:index.php");
-    exit;
-  } else {
-    echo "Error: "  . "<br>" .  mysqli_error($mysqli);
-  }
-
-}else { echo "Password Tidak Sama";
-      exit; }
-
-}
-
-}
-?>
   </body>
 </html>
