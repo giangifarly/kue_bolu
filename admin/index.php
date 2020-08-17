fa8ccfec877197c20fa0c8e2de740434c0637c9d
giangifarly <gifarlygian@gmail.com>
2020-08-18 00:53:30 +0700

    Update Link

--- admin/index.php
+++ admin/index.php
@@ -4,6 +4,76 @@
 
 <div class="container-fluid">
   <h1 class="h3 mb-4 text-gray-800">Dashboard</h1>
+
+  <div class="row">
+
+    <div class="col-xl-3 col-md-6 mb-4">
+      <a href="produk.php">
+        <div class="card border-left-primary shadow h-100 py-2">
+          <div class="card-body">
+            <div class="row no-gutters align-items-center">
+              <div class="col mr-2">
+                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Jumlah Produk</div>
+                <div class="h5 mb-0 font-weight-bold text-gray-800">448</div>
+              </div>
+              <div class="col-auto">
+                <i class="fas fa-calendar fa-2x text-gray-300"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </a>
+    </div>
+
+    <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card border-left-primary shadow h-100 py-2">
+        <div class="card-body">
+          <div class="row no-gutters align-items-center">
+            <div class="col mr-2">
+              <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Jumlah Produk</div>
+              <div class="h5 mb-0 font-weight-bold text-gray-800">2</div>
+            </div>
+            <div class="col-auto">
+              <i class="fas fa-calendar fa-2x text-gray-300"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card border-left-primary shadow h-100 py-2">
+        <div class="card-body">
+          <div class="row no-gutters align-items-center">
+            <div class="col mr-2">
+              <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Order Telah dibayar</div>
+              <div class="h5 mb-0 font-weight-bold text-gray-800">1</div>
+            </div>
+            <div class="col-auto">
+              <i class="fas fa-calendar fa-2x text-gray-300"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card border-left-primary shadow h-100 py-2">
+        <div class="card-body">
+          <div class="row no-gutters align-items-center">
+            <div class="col mr-2">
+              <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">order belum dibayar</div>
+              <div class="h5 mb-0 font-weight-bold text-gray-800">1</div>
+            </div>
+            <div class="col-auto">
+              <i class="fas fa-calendar fa-2x text-gray-300"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
 </div>
 
 <?php include "separator/footer.php" ?>
--- signup.php
+++ signup.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link href="https://fonts.googleapis.com/css?family=Permanent+Marker" rel="stylesheet">
-  </head>
-  <body>
-    <div class="signup-form">
+
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <link rel="stylesheet" href="assets/css/style.css">
+  <link href="https://fonts.googleapis.com/css?family=Permanent+Marker" rel="stylesheet">
+</head>
+
+<body>
+  <div class="signup-form">
     <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
       <h1>Sign Up</h1>
       <input type="text" name="nama" placeholder="Nama" class="txtb">
@@ -15,12 +17,13 @@
       <input type="email" name="email" placeholder="Email" class="txtb">
       <input type="password" name="passbaru1" placeholder="Masukan Password" class="txtb">
       <input type="password" name="passbaru2" placeholder="Ulangi Password" class="txtb">
-       <input type="text" name="alamat" placeholder="Alamat" class="txtb">
+      <input type="text" name="alamat" placeholder="Alamat" class="txtb">
       <input type="submit" name="submit" value="Register" class="signup-btn">
-      <a href="index.php"> 
-        <input type="button" class="btn" value="Login"> 
-      </a> 
+      <a href="login.php">
+        <input type="button" class="btn" value="Login">
+      </a>
     </form>
-    </div>
-  </body>
+  </div>
+</body>
+
 </html>
